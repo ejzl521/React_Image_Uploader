3ae87abc8d0c0376cc4671490fc2161e1935aa17
Duckgugong <55455103+ejzl521@users.noreply.github.com>
2022-07-04 00:06:36 +0900

    preview_URL을 createObjectURL로 생성

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,11 +1,10 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import "./uploader.scss";
-import { Button } from "@mui/material";
+import {Button} from "@mui/material";
 import axios from 'axios';
 
-const Uploader = (props) => {
+const Uploader = () => {
 
-  console.log(props)
   const [image, setImage] = useState({
     image_file: "",
     preview_URL: "img/default_image.png",
@@ -15,31 +14,37 @@ const Uploader = (props) => {
 
   const saveImage = (e) => {
     e.preventDefault();
-    const fileReader = new FileReader();
-    
     if(e.target.files[0]){
-      fileReader.readAsDataURL(e.target.files[0])
-    }
-    fileReader.onload = () => {
-      setImage(
+      // 새로운 이미지를 올리면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+      URL.revokeObjectURL(image.preview_URL);
+      const preview_URL = URL.createObjectURL(e.target.files[0]);
+      setImage(() => (
         {
           image_file: e.target.files[0],
-          preview_URL: fileReader.result
+          preview_URL: preview_URL
         }
-      )
+      ))
     }
-    
   }
 
   const deleteImage = () => {
+    // createObjectURL()을 통해 생성한 기존 URL을 폐기
+    URL.revokeObjectURL(image.preview_URL);
     setImage({
       image_file: "",
       preview_URL: "img/default_image.png",
     });
   }
 
+  useEffect(()=> {
+    // 컴포넌트가 언마운트되면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+    return () => {
+      URL.revokeObjectURL(image.preview_URL)
+    }
+  }, [])
+
   const sendImageToServer = async () => {
-    if(image.image_file){
+    if (image.image_file) {
       const formData = new FormData()
       formData.append('file', image.image_file);
       await axios.post('/api/image/upload', formData);
@@ -48,8 +53,7 @@ const Uploader = (props) => {
         image_file: "",
         preview_URL: "img/default_image.png",
       });
-    }
-    else{
+    } else {
       alert("사진을 등록하세요!")
     }
   }
@@ -57,15 +61,15 @@ const Uploader = (props) => {
   return (
     <div className="uploader-wrapper">
       <input type="file" accept="image/*"
-        onChange={saveImage}
+             onChange={saveImage}
         // 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
         // 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
-        onClick={(e)=>e.target.value = null}
-        ref={refParam => inputRef = refParam}
-        style={{ display: "none" }}
+             onClick={(e) => e.target.value = null}
+             ref={refParam => inputRef = refParam}
+             style={{display: "none"}}
       />
       <div className="img-wrapper">
-        <img src={image.preview_URL} />
+        <img src={image.preview_URL}/>
       </div>
 
       <div className="upload-button">
@@ -79,7 +83,6 @@ const Uploader = (props) => {
           Upload
         </Button>
       </div>
-
     </div>
   );
 }
